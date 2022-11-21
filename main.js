ae18a458e2009f762784d7c8d4b5763020fac6af
Etienne Garcia <etienne.garcia@etu.umontpellier.fr>
2022-11-21 19:37:15 +0100

    J'ai bien avancé le JS
    Le JS génère une minimap et un "joueur" avec un angle
    
    Les déplacement avec "z" et "s" fonctionne mais l'angle ne peut pas encore être modifié pour l'instant (bug)
    
    J'ai bien préparé le terrain en prévoyant mes fonctions et mes variables à l'avance

--- main.js
+++ main.js
@@ -7,20 +7,125 @@ canvas.setAttribute("width", SCREEN_WIDTH)
 canvas.setAttribute("height", SCREEN_HEIGHT)
 document.getElementById("game").appendChild(canvas)
 
+const context = canvas.getContext("2d")
+
 const TICK = 30;
 
-function clearScreen(){}
-function movePlayer(){}
-function getRays(){}
+const CELL_SIZE = 64;
+
+const PLAYER_SIZE = 10;
+
+const COLORS = {
+    rays : "#ffa600"
+}
+
+const map = [
+    [1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 1],
+    [1, 0, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1],
+];
+
+const player = {
+    x: CELL_SIZE * 1.5,
+    y: CELL_SIZE * 2,
+    angle: 0,
+    speed: 0
+}
+
+function clearScreen(){
+    context.fillStyle = "cyan"
+    context.fillRect(0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
+}
+function movePlayer(){
+    player.x += Math.cos(player.angle) * player.speed
+    player.y += Math.sin(player.angle) * player.speed
+}
+function getRays(){
+    return [];
+}
 function renderScene(rays){}
-function renderMinimap(x,y,scale,rays){}
+
+function renderMinimap(posX = 0,posY = 0,scale = 1,rays){
+    const cellSize = scale * CELL_SIZE;
+    map.forEach((row, y) => {
+        row.forEach((cell, x) => {
+            if(cell){
+                context.fillStyle = "grey"
+                context.fillRect(posX + x * cellSize,
+                    posY + y *cellSize,
+                    cellSize,
+                    cellSize
+                );
+            }
+        });
+    });
+
+    context.strokeStyle = COLORS.rays;
+    rays.forEach(ray => {
+        context.beginPath()
+        context.moveTo(player.x * scale + posX, player.y * scale + posY)
+        context.lineTo(
+            (player.x + Math.cos(ray.angle) * ray.distance) * scale,
+            (player.x + Math.cos(ray.angle) * ray.distance) * scale,
+        )
+        context.closePath()
+        context.stroke()
+    })
+    
+    context.fillStyle = "blue"
+    context.fillRect(
+        posX + player.x * scale - PLAYER_SIZE/2,
+        posY + player.y * scale - PLAYER_SIZE/2,
+        PLAYER_SIZE,
+        PLAYER_SIZE
+    )
+    
+    const rayLength = PLAYER_SIZE * 2;
+    context.strokeStyle = "blue"
+    context.beginPath()
+    context.moveTo(player.x * scale + posX, player.y * scale + posY)
+    context.lineTo(
+        (player.x + Math.cos(player.angle) * rayLength) * scale,
+        (player.y + Math.sin(player.angle) * rayLength) * scale, 
+    )
+    context.closePath()
+    context.stroke()
+    
+}
 
 function gameLoop(){
     clearScreen()
     movePlayer()
     const rays = getRays()
     renderScene(rays)
-    rendrMinimp(0,0,0.75,rays)
+    renderMinimap(0,0,0.75,rays)
 }
 
 setInterval(gameLoop,TICK)
+
+function toRadians(deg){
+    return (deg * Math.PI)/180 
+}
+
+document.addEventListener("keydown", (e)=> {
+    if(e.key=== "z"){
+        player.speed = 2
+    }
+    if(e.key=== "s"){
+        player.speed = -2
+    }
+})
+
+document.addEventListener("keyup", (e)=> {
+    if(e.key=== "z" || e.key === "s"){
+        player.speed = 0
+    }
+})
+
+/*document.addEventListener("mousemove", (e) => {
+    player.angle += toRadians(e.mouvementX)
+})*/    
