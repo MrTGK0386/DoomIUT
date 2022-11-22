0980af8faaaf7d6563a31818ee87458bb165ee8c
Etienne Garcia <etienne.garcia@etu.umontpellier.fr>
2022-11-22 16:47:53 +0100

    3D terminé
    map mise à l'échelle
    
    mur mis à l'échelle
    
    bug réglé

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 const map = [
-    [1, 1, 1, 1, 1, 1, 1],
-    [1, 0, 0, 0, 0, 1, 1],
-    [1, 0, 0, 1, 0, 0, 1],
-    [1, 0, 0, 1, 1, 0, 1],
-    [1, 0, 0, 0, 1, 0, 1],
-    [1, 0, 0, 0, 1, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 1, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    
 ];
 
 const SCREEN_WIDTH =900;
@@ -15,83 +39,85 @@ const TICK = 30;
 
 const CELL_SIZE = 32;
 
-const FOV = toRadians(60);
+const FOV = toRadians(90);
 
 const COLORS = {
-    rays : "#ffa600"
-}
+    rays : "#ffa600",
+    floor : "#1d1f1c",
+    ceiling : "#b8bab6",
+    wall : "#d3e8ca",
+    wallDark : "#a5bd9b",
+};
 
 const player = {
     x: CELL_SIZE * 1.5,
-    y: CELL_SIZE * 2,
+    y: CELL_SIZE * 15 ,
     angle: toRadians(0),
-    speed: 0
-}
+    speed: 0,
+};
 
-const canvas = document.createElement("canvas")
-canvas.setAttribute("width", SCREEN_WIDTH)
-canvas.setAttribute("height", SCREEN_HEIGHT)
+const canvas = document.createElement("canvas");
+canvas.setAttribute("width", SCREEN_WIDTH);
+canvas.setAttribute("height", SCREEN_HEIGHT);
 document.getElementById("game").appendChild(canvas)
 
-const context = canvas.getContext("2d")
+const context = canvas.getContext("2d");
 
 PLAYER_SIZE = 10;
 
-function clearScreen(){
-    context.fillStyle = "cyan"
-    context.fillRect(0,0,SCREEN_WIDTH,SCREEN_HEIGHT)
+function clearScreen() {
+    context.fillStyle = "red";
+    context.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 }
 
-function renderMinimap(posX = 0,posY = 0,scale = 1,rays){
+function renderMinimap(posX = 0, posY = 0, scale, rays) {
     const cellSize = scale * CELL_SIZE;
     map.forEach((row, y) => {
         row.forEach((cell, x) => {
-            if(cell){
-                context.fillStyle = "grey"
-                context.fillRect(posX + x * cellSize,
-                    posY + y *cellSize,
+            if (cell) {
+                context.fillStyle = "grey";
+                context.fillRect(
+                    posX + x * cellSize,
+                    posY + y * cellSize,
                     cellSize,
                     cellSize
                 );
             }
         });
     });
-
-
-
-    context.fillStyle = "blue"
+    context.fillStyle = "blue";
     context.fillRect(
-        posX + player.x * scale - PLAYER_SIZE/2,
-        posY + player.y * scale - PLAYER_SIZE/2,
+        posX + player.x * scale - PLAYER_SIZE / 2,
+        posY + player.y * scale - PLAYER_SIZE / 2,
         PLAYER_SIZE,
         PLAYER_SIZE
-    )
+    );
 
-    const rayLength = PLAYER_SIZE * 2;
-    context.strokeStyle = "blue"
-    context.beginPath()
-    context.moveTo(player.x * scale , player.y * scale )
+    const rayLenght = PLAYER_SIZE * 2;
+    context.strokeStyle = "blue";
+    context.beginPath();
+    context.moveTo(player.x * scale, player.y * scale);
     context.lineTo(
-        (player.x + Math.cos(player.angle) * rayLength) * scale,
-        (player.y + Math.sin(player.angle) * rayLength) * scale,
+        (player.x + Math.cos(player.angle) * rayLenght) * scale,
+        (player.y + Math.sin(player.angle) * rayLenght) * scale
     );
     context.closePath();
     context.stroke();
 
     context.strokeStyle = COLORS.rays;
     rays.forEach((ray) => {
-        context.beginPath()
-        context.moveTo(player.x * scale , player.y * scale );
+        context.beginPath();
+        context.moveTo(player.x * scale, player.y * scale);
         context.lineTo(
             (player.x + Math.cos(ray.angle) * ray.distance) * scale,
-            (player.y + Math.sin(ray.angle) * ray.distance) * scale,
+            (player.y + Math.sin(ray.angle) * ray.distance) * scale
         );
         context.closePath();
         context.stroke();
     });
-
 }
-function toRadians(deg){
+
+function toRadians(deg) {
     return (deg * Math.PI) / 180;
 }
 
@@ -99,13 +125,12 @@ function distance(x1, y1, x2, y2) {
     return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 }
 
-function outOfMapBounds(x,y){
+function outOfMapBounds(x, y) {
     return x < 0 || x >= map[0].length || y < 0 || y >= map.length;
 }
 
-
-function getVCollision(angle){
-    const right = Math.abs(Math.floor((angle - Math.PI / 2) / Math.PI) % 2 );
+function getVCollision(angle) {
+    const right = Math.abs(Math.floor((angle - Math.PI / 2) / Math.PI) % 2);
 
     const firstX = right
         ? Math.floor(player.x / CELL_SIZE) * CELL_SIZE + CELL_SIZE
@@ -119,81 +144,140 @@ function getVCollision(angle){
     let wall;
     let nextX = firstX;
     let nextY = firstY;
-    while(!wall){
+    while (!wall) {
         const cellX = right
             ? Math.floor(nextX / CELL_SIZE)
             : Math.floor(nextX / CELL_SIZE) - 1;
         const cellY = Math.floor(nextY / CELL_SIZE);
 
-        if(outOfMapBounds(cellX, cellY)){
+        if (outOfMapBounds(cellX, cellY)) {
             break;
         }
         wall = map[cellY][cellX];
-        if (!wall){
+        if (!wall) {
             nextX += xA;
             nextY += yA;
-        } else{
+        } else {
         }
     }
     return {
         angle,
-        distance : distance(player.x, player.y, nextX, nextY),
-        vertical : true,
+        distance: distance(player.x, player.y, nextX, nextY),
+        vertical: true,
     };
 }
 
-function castRay(angle){
-    const vCollision = getVCollision(angle);
-   //const hCollision = getHCollision(angle);
-    return vCollision;
+function getHCollision(angle) {
+    const up = Math.abs(Math.floor(angle / Math.PI) % 2);
+    const firstY = up
+        ? Math.floor(player.y / CELL_SIZE) * CELL_SIZE
+        : Math.floor(player.y / CELL_SIZE) * CELL_SIZE + CELL_SIZE;
+    const firstX = player.x + (firstY - player.y) / Math.tan(angle);
 
-    //return hCollision.distance >= vCollision.distance ? vCollision : hCollision;
+    const yA = up ? -CELL_SIZE : CELL_SIZE;
+    const xA = yA / Math.tan(angle);
+
+    let wall;
+    let nextX = firstX;
+    let nextY = firstY;
+    while (!wall) {
+        const cellX = Math.floor(nextX / CELL_SIZE);
+        const cellY = up
+            ? Math.floor(nextY / CELL_SIZE) - 1
+            : Math.floor(nextY / CELL_SIZE);
+
+        if (outOfMapBounds(cellX, cellY)) {
+            break;
+        }
+
+        wall = map[cellY][cellX];
+        if (!wall) {
+            nextX += xA;
+            nextY += yA;
+        }
+    }
+    return {
+        angle,
+        distance: distance(player.x, player.y, nextX, nextY),
+        vertical: false,
+    };
 }
 
-function getRays(){
+function castRay(angle) {
+    const vCollision = getVCollision(angle);
+    const hCollision = getHCollision(angle);
+
+    return hCollision.distance >= vCollision.distance ? vCollision : hCollision;
+}
+
+function fixFishEye(distance, angle, playerAngle) {
+    const diff = angle - playerAngle;
+    return distance * Math.cos(diff);
+}
+
+function getRays() {
     const initialAngle = player.angle - FOV / 2;
     const numberOfRays = SCREEN_WIDTH;
     const angleStep = FOV / numberOfRays;
-    return Array.from ({ length: numberOfRays}, (_, i) => {
+    return Array.from({ length: numberOfRays }, (_, i) => {
         const angle = initialAngle + i * angleStep;
         const ray = castRay(angle);
         return ray;
     });
 }
 
-function movePlayer(){
+function movePlayer() {
     player.x += Math.cos(player.angle) * player.speed;
     player.y += Math.sin(player.angle) * player.speed;
 }
 
-function renderScene(rays){}
-
-function gameLoop(){
-    clearScreen()
-    movePlayer()
-    const rays = getRays()
-    renderScene(rays)
-    renderMinimap(0,0,0.75,rays)
+function renderScene(rays) {
+    rays.forEach((ray, i) => {
+        const distance = fixFishEye(ray.distance, ray.angle, player.angle);
+        const wallHeight = ((CELL_SIZE * 5) / distance) * 100;
+        context.fillStyle = ray.vertical ? COLORS.wallDark : COLORS.wall;
+        context.fillRect(i, SCREEN_HEIGHT / 2 - wallHeight / 2, 1, wallHeight);
+        context.fillStyle = COLORS.floor;
+        context.fillRect(
+            i,
+            SCREEN_HEIGHT / 2 + wallHeight / 2,
+            1,
+            SCREEN_HEIGHT / 2 - wallHeight / 2
+        );
+        context.fillStyle = COLORS.ceiling;
+        context.fillRect(i, 0, 1, SCREEN_HEIGHT / 2 - wallHeight / 2);
+    });
 }
 
-setInterval(gameLoop,TICK)
+function gameLoop() {
+    clearScreen();
+    movePlayer();
+    const rays = getRays();
+    renderScene(rays);
+    renderMinimap(0, 0, 0.5, rays);
+}
 
+setInterval(gameLoop, TICK);
 
-document.addEventListener("keydown", (e)=> {
-    if(e.key=== "z"){
-        player.speed = 2
+canvas.addEventListener("click", () => {
+    canvas.requestPointerLock();
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.key === "z") {
+        player.speed = 2;
     }
-    if(e.key=== "s"){
-        player.speed = -2
+    if (e.key === "s") {
+        player.speed = -2;
     }
-})
+});
 
-document.addEventListener("keyup", (e)=> {
-    if(e.key=== "z" || e.key === "s"){
-        player.speed = 0
+document.addEventListener("keyup", (e) => {
+    if (e.key === "z" || e.key === "s") {
+        player.speed = 0;
     }
-})
+});
 
-document.addEventListener("mousemove", function (event){
+document.addEventListener("mousemove", function (event) {
     player.angle += toRadians(event.movementX);
-})
+});
